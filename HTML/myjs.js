31a154a0863cb25d3d544534c6dbe86a365651ca
David Spence <davidspence@Davids-MBP.fritz.box>
2021-03-16 18:40:55 +0000

    adds HTML etc files

---
+++ HTML/myjs.js
@@ -0,0 +1,9 @@
+function pictures(sw) {
+  var pic;
+  if (sw == 0) {
+    pic = "HMR_Aged21.jpg"
+  } else {
+    pic = "PHOTO-2019-07-29-18-56-18.jpg"
+  }
+  document.getElementById('myImage').src = pic;
+}
---
+++ HTML/texty.js
@@ -0,0 +1,5 @@
+function texty() {
+		var text_stuff;
+		text_stuff = document.getElementById('input_text').value;
+		document.getElementById('para').innerHTML = text_stuff;
+	}
